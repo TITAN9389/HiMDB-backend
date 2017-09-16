1b65292f65018a8bbe5978c9b748c53823e80ed8
titan9389 <titan9389@me.com>
2017-09-16 23:00:11 +0400

    added CORS

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 // ADD MOVIE
