571a031bf46f7115862f8626f8744771114feee6
titan9389 <titan9389@me.com>
2017-09-26 20:44:55 +0400

    tokenS

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,3 @@
-// var mongoose = require('mongoose');
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
-// module.exports = {mongoose};
-
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
