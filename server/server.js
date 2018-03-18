2d85d8c6fd5a3447feea634a8dc86b4b0e6c548e
Sebouh Mazloumian <titan9389@me.com>
2018-03-18 22:59:49 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    // res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
